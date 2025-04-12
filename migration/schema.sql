9b5a96889f45dc346e8be827bb2180049968d42d
Vince Scarpa <vince.in2net@gmail.com>
2025-04-12 22:03:28 +1000

    Fix: clean up
    Cleaning up forgotten file

--- migration/schema.sql
+++ migration/schema.sql
@@ -3,4 +3,4 @@ drop user 'responsible_api_user'@'%';
 CREATE USER 'responsible_api_user'@'%' IDENTIFIED BY 'responsible_api_pass';
 GRANT ALL PRIVILEGES ON responsible_api_db.* TO 'responsible_api_user'@'%';
 FLUSH PRIVILEGES;
-USE responsible_api_db;
+USE responsible_api;
